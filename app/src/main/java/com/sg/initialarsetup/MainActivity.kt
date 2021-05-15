d89c2e5af098d036b1ea3d81cf3f24648fadad43
shlomo <shlomo.gevim@gmail.com>
2021-05-15 21:52:50 +0300

    Afrer per 6. befor add fernither

--- app/src/main/java/com/sg/initialarsetup/MainActivity.kt
+++ app/src/main/java/com/sg/initialarsetup/MainActivity.kt
@@ -2,10 +2,36 @@ package com.sg.initialarsetup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.View
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.activity_main.*
+
+private const val BOOTOM_SHEET_PEEK_HIGHT = 50f
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupBottomSheet()
+    }
+
+    private fun setupBottomSheet() {
+        val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomSheetBehavior.peekHeight =
+            TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                BOOTOM_SHEET_PEEK_HIGHT,
+                resources.displayMetrics
+            ).toInt()
+        bottomSheetBehavior.addBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
+
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                bottomSheet.bringToFront()
+            }
+            override fun onStateChanged(bottomSheet: View, newState: Int) {}
+        })
     }
 }
