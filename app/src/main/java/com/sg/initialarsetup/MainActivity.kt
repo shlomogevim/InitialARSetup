dd1e2439eef11e6c6346b3ae589820c4a0f41ba6
shlomo <shlomo.gevim@gmail.com>
2021-05-17 10:25:09 +0300

    Per 10 is completted

--- app/src/main/java/com/sg/initialarsetup/MainActivity.kt
+++ app/src/main/java/com/sg/initialarsetup/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.TypedValue
 import android.view.View
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,10 +17,9 @@ class MainActivity : AppCompatActivity() {
         Model(R.drawable.oven,"Oven",R.raw.oven),
         Model(R.drawable.piano,"Piano",R.raw.piano),
         Model(R.drawable.table,"Table",R.raw.table)
-
-
-
     )
+    private lateinit var selectedModel:Model
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -28,7 +28,13 @@ class MainActivity : AppCompatActivity() {
     }
     private fun setupRecyclerView(){
         rvModels.layoutManager=LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
-        rvModels.adapter=ModelAdapter(models)
+        rvModels.adapter=ModelAdapter(models).apply {
+            selecteModel.observe(this@MainActivity, Observer {
+                this@MainActivity.selectedModel=it
+                val newTitle="Models (${it.title})"
+                tvModel.text=newTitle
+            })
+        }
     }
 
     private fun setupBottomSheet() {
--- app/src/main/java/com/sg/initialarsetup/ModelAdapter.kt
+++ app/src/main/java/com/sg/initialarsetup/ModelAdapter.kt
@@ -1,12 +1,20 @@
 package com.sg.initialarsetup
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_model.view.*
 
+const val SELECTED_MODEL_COLOR=Color.YELLOW
+const val UNSELECTED_MODEL_COLOR=Color.LTGRAY
+
 class ModelAdapter(val models:List<Model>):RecyclerView.Adapter<ModelAdapter.ModelViewHolder>() {
+
+    var selecteModel=MutableLiveData<Model>()
+    private var selectedModelIndex=0
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ModelViewHolder {
@@ -15,15 +23,32 @@ class ModelAdapter(val models:List<Model>):RecyclerView.Adapter<ModelAdapter.Mod
     }
 
     override fun onBindViewHolder(holder: ModelViewHolder, position: Int) {
+        if (selectedModelIndex==holder.layoutPosition){
+            holder.itemView.setBackgroundColor(SELECTED_MODEL_COLOR)
+            selecteModel.value=models[holder.layoutPosition]
+        }else{
+            holder.itemView.setBackgroundColor(UNSELECTED_MODEL_COLOR)
+        }
         holder.itemView.apply {
             ivThumnail.setImageResource(models[position].imageResourceId)
             tvTitle.text=models[position].title
+            setOnClickListener {
+                selectedMode(holder)
+            }
         }
     }
 
     override fun getItemCount()=models.size
 
-    inner class ModelViewHolder(itemView:View):RecyclerView.ViewHolder(itemView){
+    inner class ModelViewHolder(itemView:View):RecyclerView.ViewHolder(itemView){ }
 
+    private fun selectedMode(holder: ModelViewHolder){
+        if (selectedModelIndex!=holder.layoutPosition){
+            holder.itemView.setBackgroundColor(SELECTED_MODEL_COLOR)
+            notifyItemChanged(selectedModelIndex)
+            selectedModelIndex=holder.layoutPosition
+            selecteModel.value=models[holder.layoutPosition]
+        }
     }
+
 }
